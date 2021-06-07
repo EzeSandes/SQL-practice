bb3de91404d4aeb5b994de1aed5d0e380a916a01
Eze Sandes <jezesandes@gmail.com>
2021-06-07 12:35:12 -0300

    Added Exerc 2

---
+++ Ejerc-2-SQL/Ejerc-2-CreateRegs.sql
@@ -0,0 +1,147 @@
+-- Proveedor (NroProv, NomProv, Categoria, CiudadProv)
+
+CREATE DATABASE Ejerc2;
+USE Ejerc2;
+
+CREATE TABLE proveedor
+(	nroProv INT NOT NULL,
+    nomProv VARCHAR(50) NOT NULL,
+    categoria INT,
+    ciudadProv VARCHAR(50),
+    PRIMARY KEY(nroProv)
+);
+
+-- Artículo (NroArt, Descripción, CiudadArt, Precio)
+
+CREATE TABLE articulo
+(
+	nroArt VARCHAR(10) NOT NULL,
+    descripcion VARCHAR(50),
+    ciudadArt VARCHAR(50),
+    precio INT, -- Para simplificar el ejerc, que sean ENTEROS
+    PRIMARY KEY(nroArt)
+);
+
+-- Cliente (NroCli, NomCli, CiudadCli)
+
+CREATE TABLE cliente
+(
+	nroCli VARCHAR(10) NOT NULL,
+    nomCli VARCHAR(50),
+    ciudadCli VARCHAR(50),
+    PRIMARY KEY(nroCli)
+);
+
+-- Pedido (NroPed, NroArt, NroCli, NroProv, FechaPedido, Cantidad, PrecioTotal)
+
+CREATE TABLE pedido
+(
+	nroPed INT NOT NULL,
+    nroArt VARCHAR(10),
+    nroCli VARCHAR(10),
+    nroProv INT,
+    fechaPedido DATE,
+    cantidad INT,
+    precioTotal INT,
+    PRIMARY KEY(nroPed),
+    FOREIGN KEY(nroArt) REFERENCES articulo(nroArt),
+    FOREIGN KEY(nroCli) REFERENCES cliente(nroCli),
+    FOREIGN KEY(nroProv) REFERENCES proveedor(nroProv)
+);
+
+-- Stock (NroArt, fecha, cantidad)
+
+CREATE TABLE stock
+(
+	nroArt varchar(50),
+    fecha DATE,
+    cantidad int,
+    PRIMARY KEY(nroArt, fecha),
+    foreign key(nroArt) REFERENCES articulo(nroArt)
+);
+DROP TABLE stock;
+
+-- ///////////////////////// INSERTS
+
+INSERT INTO proveedor(nroProv, nomProv, categoria, ciudadProv) VALUES (1, "Juan Perez", 1, "CABA");
+INSERT INTO proveedor(nroProv, nomProv, categoria, ciudadProv) VALUES (2, "Maria Lopez", 3, "La Plata");
+INSERT INTO proveedor(nroProv, nomProv, categoria, ciudadProv) VALUES (3, "Jose Nunez", 3, "Gonzalez Catan");
+INSERT INTO proveedor(nroProv, nomProv, categoria, ciudadProv) VALUES (4, "Agustin Estrada", 4, "CABA");
+INSERT INTO proveedor(nroProv, nomProv, categoria, ciudadProv) VALUES (5, "Donald Trump", 1, "Rosario");
+INSERT INTO proveedor(nroProv, nomProv, categoria, ciudadProv) VALUES (6, "Max Power", 3, "Montevideo");
+INSERT INTO proveedor(nroProv, nomProv, categoria, ciudadProv) VALUES (7, "Carla Rodriguez", 1, "Cordoba");
+
+
+
+INSERT INTO articulo(nroArt, descripcion, ciudadArt, precio) VALUES ("a146", "lapiz", "Rosario", 20);
+INSERT INTO articulo(nroArt, descripcion, ciudadArt, precio) VALUES ("a250", "cuaderno", "Mendoza", 50);
+INSERT INTO articulo(nroArt, descripcion, ciudadArt, precio) VALUES ("b130", "portalapiz", "CABA", 10);
+INSERT INTO articulo(nroArt, descripcion, ciudadArt, precio) VALUES ("d456", "block 30 hojas", "La Plata", 30);
+INSERT INTO articulo(nroArt, descripcion, ciudadArt, precio) VALUES ("c490", "Resaltador", "Mendoza", 80);
+INSERT INTO articulo(nroArt, descripcion, ciudadArt, precio) VALUES ("e569", "Mochila", "CABA", 200);
+INSERT INTO articulo(nroArt, descripcion, ciudadArt, precio) VALUES ("e236", "Lampara", "Cordoba", 300);
+INSERT INTO articulo(nroArt, descripcion, ciudadArt, precio) VALUES ("a123", "cartuchera", "La Plata", 60);
+
+
+INSERT INTO cliente(nroCli, nomCli, ciudadCli) VALUES ("c23", "Fereico Almendra", "Mendoza");
+INSERT INTO cliente(nroCli, nomCli, ciudadCli) VALUES ("c30", "MARÍA DOLORES", "Rosario");
+INSERT INTO cliente(nroCli, nomCli, ciudadCli) VALUES ("c25", "JUAN MIGUEL", "CABA");
+INSERT INTO cliente(nroCli, nomCli, ciudadCli) VALUES ("c15", "LAURA SUSANA", "CABA");
+INSERT INTO cliente(nroCli, nomCli, ciudadCli) VALUES ("c60", "SARA GLORIA", "Rosario");
+INSERT INTO cliente(nroCli, nomCli, ciudadCli) VALUES ("c80", "CARLOS ALBERTO", "La Plata");
+INSERT INTO cliente(nroCli, nomCli, ciudadCli) VALUES ("c90", "MARÍA LUCINDA", "Mendoza");
+INSERT INTO cliente(nroCli, nomCli, ciudadCli) VALUES ("c35", "CARLOS ALFONSO", "CABA");
+INSERT INTO cliente(nroCli, nomCli, ciudadCli) VALUES ("c40", "FRANCISCO JAVIER", "CABA");
+INSERT INTO cliente(nroCli, nomCli, ciudadCli) VALUES ("c45", "ALBERTO MANUEL", "CABA");
+
+
+
+-- -------------------------------
+INSERT INTO pedido(nroPed, nroArt, nroCli, nroProv, fechaPedido, cantidad, precioTotal) VALUES (1, "a146", "c23", 1, "2004-01-01", 10, 1000);
+INSERT INTO pedido(nroPed, nroArt, nroCli, nroProv, fechaPedido, cantidad, precioTotal) VALUES (2, "e236", "c23", 2, "2004-02-01", 20, 2000);
+INSERT INTO pedido(nroPed, nroArt, nroCli, nroProv, fechaPedido, cantidad, precioTotal) VALUES (3, "a123", "c15", 5, "2004-03-31", 10, 500);
+INSERT INTO pedido(nroPed, nroArt, nroCli, nroProv, fechaPedido, cantidad, precioTotal) VALUES (4, "e236", "c60", 1, "2004-01-01", 50, 2000);
+INSERT INTO pedido(nroPed, nroArt, nroCli, nroProv, fechaPedido, cantidad, precioTotal) VALUES (5, "c490", "c35", 1, "2004-03-31", 30, 1000);
+INSERT INTO pedido(nroPed, nroArt, nroCli, nroProv, fechaPedido, cantidad, precioTotal) VALUES (6, "a146", "c40", 3, "2004-02-01", 20, 2000);
+INSERT INTO pedido(nroPed, nroArt, nroCli, nroProv, fechaPedido, cantidad, precioTotal) VALUES (7, "e236", "c15", 4, "2004-01-01", 10, 1000);
+INSERT INTO pedido(nroPed, nroArt, nroCli, nroProv, fechaPedido, cantidad, precioTotal) VALUES (8, "d456", "c60", 5, "2004-02-01", 10, 500);
+INSERT INTO pedido(nroPed, nroArt, nroCli, nroProv, fechaPedido, cantidad, precioTotal) VALUES (9, "a146", "c23", 1, "2004-02-01", 50, 2000);
+INSERT INTO pedido(nroPed, nroArt, nroCli, nroProv, fechaPedido, cantidad, precioTotal) VALUES (10, "e236", "c40", 3, "2004-03-31", 10, 1000);
+INSERT INTO pedido(nroPed, nroArt, nroCli, nroProv, fechaPedido, cantidad, precioTotal) VALUES (11, "c490", "c23", 6, "2004-01-01", 50, 2000);
+INSERT INTO pedido(nroPed, nroArt, nroCli, nroProv, fechaPedido, cantidad, precioTotal) VALUES (12, "a146", "c15", 4, "2004-01-01", 100, 3000);
+INSERT INTO pedido(nroPed, nroArt, nroCli, nroProv, fechaPedido, cantidad, precioTotal) VALUES (13, "c490", "c30", 3, "2004-02-01", 10, 500);
+INSERT INTO pedido(nroPed, nroArt, nroCli, nroProv, fechaPedido, cantidad, precioTotal) VALUES (14, "d456", "c30", 1, "2004-01-01", 30, 1000);
+
+INSERT INTO pedido(nroPed, nroArt, nroCli, nroProv, fechaPedido, cantidad, precioTotal) VALUES (15, "a123", "c80", 1, "2004-01-01", 20, 100);
+INSERT INTO pedido(nroPed, nroArt, nroCli, nroProv, fechaPedido, cantidad, precioTotal) VALUES (16, "a250", "c80", 1, "2004-02-01", 10, 50);
+INSERT INTO pedido(nroPed, nroArt, nroCli, nroProv, fechaPedido, cantidad, precioTotal) VALUES (17, "e569", "c45", 1, "2004-01-01", 5, 100);
+
+INSERT INTO pedido(nroPed, nroArt, nroCli, nroProv, fechaPedido, cantidad, precioTotal) VALUES (18, "e569", "c90", 6, "2004-02-01", 10, 50);
+
+INSERT INTO pedido(nroPed, nroArt, nroCli, nroProv, fechaPedido, cantidad, precioTotal) VALUES (19, "d456", "c60", 4, "2004-02-01", 10, 100);
+INSERT INTO pedido(nroPed, nroArt, nroCli, nroProv, fechaPedido, cantidad, precioTotal) VALUES (20, "d456", "c30", 6, "2004-02-01", 10, 50);
+
+INSERT INTO pedido(nroPed, nroArt, nroCli, nroProv, fechaPedido, cantidad, precioTotal) VALUES (21, "e236", "c15", 3, "2004-03-31", 1000, 100);
+INSERT INTO pedido(nroPed, nroArt, nroCli, nroProv, fechaPedido, cantidad, precioTotal) VALUES (22, "d456", "c40", 5, "2004-02-01", 100, 200);
+-- INSERT INTO pedido(nroPed, nroArt, nroCli, nroProv, fechaPedido, cantidad, precioTotal) VALUES (, "", "", , "", , );
+
+
+-- /////////////
+
+INSERT INTO stock(nroArt, fecha, cantidad) VALUES ("a146", "2004-01-01", 100);
+INSERT INTO stock(nroArt, fecha, cantidad) VALUES ("a250", "2004-02-01", 100);
+INSERT INTO stock(nroArt, fecha, cantidad) VALUES ("b130", "2004-02-01", 200);
+INSERT INTO stock(nroArt, fecha, cantidad) VALUES ("d456", "2004-01-01", 50);
+INSERT INTO stock(nroArt, fecha, cantidad) VALUES ("c490", "2004-01-01", 200);
+INSERT INTO stock(nroArt, fecha, cantidad) VALUES ("e569", "2004-01-01", 100);
+INSERT INTO stock(nroArt, fecha, cantidad) VALUES ("e236", "2004-01-01", 50);
+INSERT INTO stock(nroArt, fecha, cantidad) VALUES ("a123", "2004-01-01", 100);
+
+INSERT INTO stock(nroArt, fecha, cantidad) VALUES ("d456", "2004-02-01", 50);
+
+
+
+
+
+
+
---
+++ Ejerc-2-SQL/Ejerc-2.sql
@@ -0,0 +1,237 @@
+USE ejerc2;
+
+-- 1. Hallar el código (nroProv) de los proveedores que proveen el artículo a146.
+
+SELECT DISTINCT nroProv
+FROM pedido
+WHERE nroArt = 'a146';
+
+-- 2. Hallar los clientes (nomCli) que solicitan artículos provistos por p015(yo elijo el 5).
+
+SELECT nroCli
+FROM pedido
+WHERE nroArt IN (
+					SELECT nroArt
+                    FROM pedido
+                    WHERE nroProv = 5
+				);
+
+-- 3. Hallar los clientes que solicitan algún item provisto por proveedores con categoría
+	-- mayor que 4(yo tomo >= 3).
+
+SELECT DISTINCT nroCli
+FROM pedido
+WHERE nroProv IN 	(	SELECT nroProv
+						FROM proveedor
+                        WHERE categoria >= 3
+					);
+
+-- 4. Hallar los pedidos en los que un cliente de Rosario solicita artículos producidos en
+	-- la ciudad de Mendoza.
+
+SELECT *
+FROM pedido
+WHERE nroCli IN (
+						SELECT nroCli
+                        FROM cliente
+                        WHERE ciudadCli = 'Rosario'
+						)
+		AND
+        nroArt IN (
+					SELECT nroArt
+					FROM articulo
+					WHERE ciudadArt = 'Mendoza'
+				)
+;
+
+-- 5. Hallar los pedidos en los que el cliente c23 solicita artículos solicitados por el
+	-- cliente c30.
+
+-- BIEN
+SELECT DISTINCT p1.*
+FROM pedido p1, pedido p2
+WHERE p1.nroCli = 'c30' AND p2.nroCli = 'c23' AND p2.nroArt = p1.nroArt
+;
+
+SELECT *
+FROM pedido 
+WHERE nroCli = 'c23';
+
+SELECT *
+FROM pedido 
+WHERE nroCli = 'c30';
+
+
+-- 6. Hallar los proveedores que suministran todos los artículos cuyo precio es superior
+	-- al precio promedio de los artículos que se producen en La Plata.
+
+# Hallar los preveedores tales que NO EXISTE articulos cuyo precio es superior al precio promedio de los articulos
+# que se preoducen en La Plata que NO sean suministrados.
+
+SELECT nroArt
+FROM articulo 
+WHERE precio > (SELECT AVG(precio) FROM articulo WHERE ciudadArt = 'La Plata');
+
+SELECT precio FROM articulo WHERE ciudadArt = 'La Plata';
+
+#BIEN. El proveedor 1 es el unico que suministra TODOS los articulos cuyo precio es superior al promedio....
+SELECT nroProv
+FROM proveedor
+WHERE NOT EXISTS (	SELECT *
+					FROM articulo 
+					WHERE precio > (SELECT AVG(precio) FROM articulo WHERE ciudadArt = 'La Plata')
+                    AND NOT EXISTS (	SELECT *
+										FROM pedido
+                                        WHERE 	proveedor.nroProv = pedido.nroProv
+												AND articulo.nroArt = pedido.nroArt
+									)
+				 )
+;
+
+-- 7. Hallar la cantidad de artículos diferentes provistos por cada proveedor que provee
+	-- a todos los clientes de Junín(yo utilizo Mendoza).
+
+# provee a todos los clientes de Junín(yo utilizo Mendoza)
+
+#RTA: nro Prov: 6. Es quien provee a TODOS los clientes de mendoza
+
+-- Proveedores que provee a TODOS los clientes de Mendoza
+SELECT nroProv
+FROM proveedor
+WHERE NOT EXISTS (	SELECT *
+					FROM cliente
+                    WHERE ciudadCli = 'Mendoza'
+                    AND NOT EXISTS (	SELECT *
+										FROM pedido
+                                        WHERE	pedido.nroCli = cliente.nroCli
+												AND proveedor.nroProv = pedido.nroProv
+									)
+					)
+;
+--
+# BIEN
+SELECT nroProv, COUNT(*) AS "Cant Art dif"
+FROM pedido
+WHERE nroProv IN 
+(
+SELECT nroProv
+FROM proveedor
+WHERE NOT EXISTS (	SELECT *
+					FROM cliente
+                    WHERE ciudadCli = 'Mendoza'
+                    AND NOT EXISTS (	SELECT *
+										FROM pedido
+                                        WHERE	pedido.nroCli = cliente.nroCli
+												AND proveedor.nroProv = pedido.nroProv
+									)
+					)
+)
+GROUP BY nroProv
+HAVING COUNT(*)
+;
+
+-- 8. Hallar los nombres de los proveedores cuya categoría sea mayor que la de todos
+	-- los proveedores que proveen el artículo “cuaderno”.
+
+;
+# Todos los proveedores que proveen el articulo 'Cuaderno' y su categoria
+SELECT DISTINCT proveedor.nroProv, categoria
+FROM pedido, proveedor
+WHERE nroArt = (	SELECT nroArt
+					FROM articulo
+                    WHERE descripcion = 'cuaderno'
+				)
+	AND pedido.nroProv = proveedor.nroProv
+;
+
+#BIEN
+SELECT nomProv
+FROM proveedor p1
+WHERE p1.categoria > ALL 
+(
+	SELECT DISTINCT categoria
+	FROM pedido, proveedor
+	WHERE nroArt = (	SELECT nroArt
+						FROM articulo
+						WHERE descripcion = 'cuaderno'
+					)
+			AND pedido.nroProv = proveedor.nroProv
+)
+;
+
+-- 9. Hallar los proveedores que han provisto más de 1000 unidades entre los artículos
+	-- A001y A100.(...mas de 50 unidades entre los articulos a250 y c490)
+/* TO DO: Exerc 9
+SELECT prov.nroProv
+FROM proveedores prov, pedido ped
+WHERE	prov.nroProv = ped.nroProv
+		AND 
+ ;       
+	
+# EN VERDAD. NI IDEA DE LO QUE PIDE MAS QUE DE LA RESOLUCION
+SELECT SUM(cantidad)
+FROM pedido
+WHERE nroArt = 'a250' OR nroArt = 'c490';
+*/
+
+-- 10. Listar la cantidad y el precio total de cada artículo que han pedido los Clientes a
+	-- sus proveedores entre las fechas 01-01-2004 y 31-03-2004 (se requiere visualizar
+	-- Cliente, Articulo, Proveedor, Cantidad y Precio).
+
+# Nota/To Do: Funciona, pero, si agrupo por 'nroProv' => no mostrara las cantidades totales y el precio total. Corregir esto
+SELECT nroCli, nroArt, /*nroProv,*/ SUM(cantidad) AS Cant_total, SUM(precioTotal) AS Prec_Total
+FROM pedido p
+WHERE fechaPedido BETWEEN '2004-01-01' AND '2004-03-31'
+GROUP BY nroArt, nroCli
+ORDER BY nroCli, nroArt;
+
+
+-- 11. Idem anterior y que además la Cantidad sea mayor o igual a 1000 o el Precio sea
+	-- mayor a $ 1000. (Yo utilice, por mi lote de prueba, cantidad >= 1000 y precio > 1000 )
+
+SELECT nroCli, nroArt, /*nroProv,*/ SUM(cantidad) AS Cant_total, SUM(precioTotal) AS Prec_Total
+FROM pedido p
+WHERE	fechaPedido BETWEEN '2004-01-01' AND '2004-03-31'
+GROUP BY nroArt, nroCli
+HAVING SUM(cantidad) >= 1000 OR SUM(precioTotal) > 1000
+ORDER BY nroCli, nroArt;
+
+-- 12. Listar la descripción de los artículos en donde se hayan pedido en el día más del
+	-- stock existente para ese mismo día.
+
+SELECT DISTINCT a.descripcion, pe.fechaPedido /*fecha Pedido para verificar que son de distintos dias.*/
+FROM articulo a, stock s, pedido pe
+WHERE	a.nroArt = pe.nroArt 
+		AND s.nroArt = pe.nroArt
+		AND pe.fechaPedido = s.fecha
+        AND s.cantidad < (	SELECT SUM(cantidad)
+							FROM pedido pe2
+                            WHERE	a.nroArt = pe2.nroArt
+									AND pe2.fechaPedido = pe.fechaPedido
+						 )
+;
+
+
+-- 13. Listar los datos de los proveedores que hayan pedido de todos los artículos en un
+	-- mismo día. Verificar sólo en el último mes de pedidos.
+
+# TO DO
+/*
+SELECT prov.*
+FROM proveedor prov
+WHERE NOT EXISTS (	SELECT 1
+					FROM articulo a
+                    WHERE NOT EXISTS (	SELECT 1
+										FROM pedido pe
+                                        WHERE	prov.nroProv = pe.nroProv
+												AND a.nroArt = pe.nroArt
+									 )
+				 )
+*/
+
+
+
+
+
+
+
